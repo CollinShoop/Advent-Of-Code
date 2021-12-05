76e62fdd4ad86840068746f315ae7d9eefb27c45
CollinShoop <cws55854@gmail.com>
2021-12-04 23:00:23 -0500

    --- Day 4: Giant Squid --- Another solution

--- cmd/4/day4.go
+++ cmd/4/day4.go
@@ -4,9 +4,115 @@ import (
 	"fmt"
 	"github.com/collinshoop/adventofcode2021/internal/util"
 	"strings"
+	"time"
 )
 
 func main() {
+	{
+		fmt.Println("Basic Solution")
+		start := time.Now()
+		basicSolution()
+		fmt.Println("Basic Solution Took", time.Since(start))
+	}
+	fmt.Println()
+	{
+		fmt.Println("Optimized Solution")
+		start := time.Now()
+		optimizedSolution()
+		fmt.Println("Optimized Solution Took", time.Since(start))
+	}
+}
+
+func optimizedSolution() {
+	/*
+		A summary of the approach taken here:
+		1. re-map each bingo tile to the move in which that value is called
+		2. find the number of moves needed to "Bingo!" each board by finding the minimum of maximums of each row/col
+		3. keeping track of the board taking minimum and maximum number of moves to "Bingo!"
+		4. score only the min/max boards by summing tiles with a move index >= number of moves required to "Bingo!"
+
+		In total this performs ~15-20x, excluding reading in the input moves and boards.
+	*/
+
+	inputs := util.ReadLines("inputs/day4.txt")
+	moves := strings.Split(inputs[0], ",")
+	boards := parseBoards(inputs[1:])
+
+	// remap moves to the turn in which that move is called
+	moveToCall := map[string]int{}
+	for i, m := range moves {
+		moveToCall[m] = i
+	}
+
+	// remap boards to called spaced
+	for _, board := range boards {
+		board.tilesd = make([][]int, 5)
+		for i := 0; i < 5; i++ {
+			board.tilesd[i] = make([]int, 5)
+			for j := 0; j < 5; j++ {
+				board.tilesd[i][j] = moveToCall[board.tiles[i][j]]
+			}
+		}
+	}
+
+	minBoard := &board{
+		moves: 100,
+	}
+	maxBoard := &board{
+		moves: 1,
+	}
+
+	// go through each board and find the number of moves it takes to win
+	for _, board := range boards {
+		minMovesToBingo := 100
+		for i := 0; i < 5; i++ {
+			maxMovesPerRow := 0
+			maxMovesPerCol := 0
+			for j := 0; j < 5; j++ {
+				moveByRow := board.tilesd[i][j]
+				if moveByRow > maxMovesPerRow {
+					maxMovesPerRow = moveByRow
+				}
+				moveByCol := board.tilesd[j][i]
+				if moveByCol > maxMovesPerCol {
+					maxMovesPerCol = moveByCol
+				}
+			}
+			if maxMovesPerCol < minMovesToBingo {
+				minMovesToBingo = maxMovesPerCol
+			}
+			if maxMovesPerRow < minMovesToBingo {
+				minMovesToBingo = maxMovesPerRow
+			}
+		}
+		board.moves = minMovesToBingo
+		if board.moves > maxBoard.moves {
+			maxBoard = board
+		}
+		if board.moves < minBoard.moves {
+			minBoard = board
+		}
+	}
+
+	score := func(b *board) {
+		score := 0
+		for i := 0; i < 5; i++ {
+			for j := 0; j < 5; j++ {
+				if b.tilesd[i][j] > b.moves {
+					score += util.ParseInt(b.tiles[i][j])
+				}
+			}
+		}
+		finalMove := moves[b.moves]
+		fmt.Printf("Bingo on move %s with a subscore of %d and total score of %d\n", finalMove, score, score*util.ParseInt(finalMove))
+	}
+	fmt.Println("Min board:", minBoard.moves)
+	score(minBoard)
+	fmt.Println("Max board:", maxBoard.moves)
+	score(maxBoard)
+}
+
+func basicSolution() {
 	inputs := util.ReadLines("inputs/day4.txt")
 	moves := strings.Split(inputs[0], ",")
 	boards := parseBoards(inputs[1:])
@@ -33,16 +139,25 @@ func main() {
 			}
 		}
 	}
-
 }
 
 type board struct {
-	moves int
-	tiles [][]string
+	n      int
+	moves  int
+	tiles  [][]string
+	tilesd [][]int
 }
 
 func (b *board) print() {
+	fmt.Println("Board", b.n)
 	for _, row := range b.tiles {
+		fmt.Println(row)
+	}
+}
+
+func (b *board) printd() {
+	fmt.Println("Board", b.n)
+	for _, row := range b.tilesd {
 		fmt.Println(row)
 	}
 }
@@ -80,12 +195,13 @@ func (b *board) bingo() (bingo bool, score int) {
 func parseBoards(inputs []string) []*board {
 	boards := []*board{}
 	for i := 0; i < len(inputs); i += 6 {
-		puzzleInput := inputs[i+1 : i+6]
+		boardRawLines := inputs[i+1 : i+6]
 		b := &board{
-			tiles: [][]string{},
+			n:     len(boards),
+			tiles: make([][]string, 5)[:0],
 		}
-		for _, line := range puzzleInput {
-			row := []string{}
+		for _, line := range boardRawLines {
+			row := make([]string, 5)[:0]
 			for j := 0; j < 14; j += 3 {
 				row = append(row, strings.TrimSpace(line[j:j+2]))
 			}
